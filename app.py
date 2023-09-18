d6dd7eb2eb03affd734b0036c066689a080ce0f1
Rick Mondal <124441126+rickmondal-2004@users.noreply.github.com>
2023-09-18 14:25:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import openai
 app = Flask(__name__)
 
 # Set up OpenAI API credentials
-openai.api_key = 'API Key'
+openai.api_key = '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'
 
 
 # Define the default route to return the index.html file
